418854497d106809f39b06c88ec912254ee6e11d
roya parsa <99053997+rparsa49@users.noreply.github.com>
2023-12-08 06:59:39 -0500

    added registration php

--- registration.php
+++ registration.php
@@ -8,24 +8,86 @@
     2.5 if yes, then display error that says "account already exists, please log in" with button to nav to log in
     3. if doesnt exist already, then add to DB and navigate to correct screen based on user type -->
     <p>
-        <?php
-            $txt = "Attempt to register...";
-            echo $txt;
+<?php
+    $txt = "Attempt to register...";
+    echo $txt;
 
-            $servername = "localhost";
-            $username = "root";
-            $password = "AlfieHershey";
-            $port = "3308";
-            $dbname = "psirt";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $role = $_POST["role"];
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $userName = $_POST["userName"];
+        $email = $_POST["email"];
+        $phoneNumber = $_POST["phoneNumber"];
+        $address = $_POST["address"];
+        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+        
+        // Add the rest of your form fields...
 
-            $conn = new mysqli($servername, $username, $password, $dbname, $port);
+        // TODO: Validate and sanitize user inputs
 
-            if ($conn->connect_error) {
-                die("<p style='color:red'>" . "Connection failed: " . $conn->connect_error . "</p>");
-            }
+        $servername = "localhost";
+        $username = "root";
+        $password = "AlfieHershey";
+        $port = "3308";
+        $dbname = "psirt";
 
-            echo "<br> Successfully connected to DB...";
-        ?>
+        $conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+        if ($conn->connect_error) {
+            die("<p style='color:red'>" . "Connection failed: " . $conn->connect_error . "</p>");
+        }
+
+        echo "<br> Successfully connected to DB...";
+
+        // Check if the email already exists
+        $checkEmailStmt = $conn->prepare("SELECT userID FROM user WHERE emailAddress = ?");
+        $checkEmailStmt->bind_param("s", $email);
+        $checkEmailStmt->execute();
+        $checkEmailResult = $checkEmailStmt->get_result();
+
+        if ($checkEmailResult->num_rows > 0) {
+            echo "<p style='color:red'>User with this email already exists. Please log in.</p>";
+            $checkEmailStmt->close();
+            $conn->close();
+            // Redirect to login page or display a login link
+            header("Location: login.php");
+            exit(); // Stop further execution
+        }
+
+        $checkEmailStmt->close();
+
+        // If the email doesn't exist, proceed with registration
+        $registerStmt = $conn->prepare("INSERT INTO user (firstname, lastname, type, address, phoneNumber, username, password, emailAddress, ipAddress) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $registerStmt->bind_param("sssssssss", $firstName, $lastName, $role, $address, $phoneNumber, $userName, $password, $email, $ipAddress);
+
+        if ($registerStmt->execute()) {
+            echo "<p style='color:green'>User registered successfully!</p>";
+            header("Location: landing.html");
+            exit();
+            // Redirect to the appropriate page based on user type
+            // switch ($role) {
+            //     case 'client':
+            //         header("Location: client_dashboard.php");
+            //         break;
+            //     case 'sitter':
+            //         header("Location: sitter_dashboard.php");
+            //         break;
+            //     case 'handler':
+            //         header("Location: handler_dashboard.php");
+            //         break;
+            //     default:
+            //         // Redirect to a default page or display an error
+            //         header("Location: default_dashboard.php");
+            // }
+        } else {
+            echo "<p style='color:red'>Error registering user: " . $registerStmt->error . "</p>";
+        }
+
+        $registerStmt->close();
+        $conn->close();
+    }
+?>
     </p>
 </body>
 </html>
