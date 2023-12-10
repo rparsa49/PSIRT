22ff2938541ecc07baff6c08821737a9d0221171
roya parsa <99053997+rparsa49@users.noreply.github.com>
2023-12-10 06:31:32 -0500

    client dash skeleton completed, added functionality for client to request order, made templates for other dashes + php for client

--- registration.php
+++ registration.php
@@ -28,69 +28,102 @@
 
 <body>
     <h1>PSIRT User Registration</h1>
-    <?php
-    
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "AlfieHershey";
-    $dbname = "psirt";
-    $port = "3308";
+session_start(); // Start the session
 
-    $conn = new mysqli($servername, $username, $password, $dbname, $port);
+$servername = "localhost";
+$username = "root";
+$password = "AlfieHershey";
+$dbname = "psirt";
+$port = "3308";
 
-    if ($conn->connect_error) {
-        die("<p class='error'>" . "Connection Failed: " . $conn->connect_error . "</p>");
-    }
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $role = $_POST['role'];
-        $firstName = $_POST['firstName'];
-        $lastName = $_POST['lastName'];
-        $userName = $_POST['userName'];
-        $email = $_POST['email'];
-        $phoneNumber = $_POST['phoneNumber'];
-        $address = $_POST['address'];
-        $password = $_POST['password'];
-        $ipAddress = $_SERVER['REMOTE_ADDR'];
+if ($conn->connect_error) {
+    die("<p class='error'>" . "Connection Failed: " . $conn->connect_error . "</p>");
+}
 
-        // Check if the email already exists
-        $checkEmailStmt = $conn->prepare("SELECT userID FROM user WHERE emailAddress = ?");
-        $checkEmailStmt->bind_param("s", $email);
-        $checkEmailStmt->execute();
-        $checkEmailResult = $checkEmailStmt->get_result();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $role = $_POST['role'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $userName = $_POST['userName'];
+    $email = $_POST['email'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $address = $_POST['address'];
+    $password = $_POST['password'];
+    $ipAddress = $_SERVER['REMOTE_ADDR'];
 
-        if ($checkEmailResult->num_rows > 0) {
-            echo "<p class='error'>User with this email already exists. Please log in.</p>";
-            $checkEmailStmt->close();
-            $conn->close();
-            // Redirect to login page or display a login link
-            header("Location: login.php");
-            exit(); // Stop further execution
-        }
+    // Check if the email already exists
+    $checkEmailStmt = $conn->prepare("SELECT userID FROM user WHERE emailAddress = ?");
+    $checkEmailStmt->bind_param("s", $email);
+    $checkEmailStmt->execute();
+    $checkEmailResult = $checkEmailStmt->get_result();
 
+    if ($checkEmailResult->num_rows > 0) {
+        echo "<p class='error'>User with this email already exists. Please log in.</p>";
         $checkEmailStmt->close();
-
-        // If the email doesn't exist, proceed with registration
-        $registerStmt = $conn->prepare("INSERT INTO user (firstname, lastname, type, address, phoneNumber, username, password, emailAddress, ipAddress) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $registerStmt->bind_param("sssssssss", $firstName, $lastName, $role, $address, $phoneNumber, $userName, $password, $email, $ipAddress);
-
-        // Attempt to register
-        if ($registerStmt->execute()) {
-            echo "<p class='success'>User registered successfully!</p>";
-            // TODO: implement that it will direct to the proper dashboard based on which role they are
-            exit();
-        } else {
-            echo "<p class='error'>Error registering user: " . $registerStmt->error . "</p>";
-        }
-
-        $registerStmt->close();
+        $conn->close();
+        // Redirect to login page or display a login link
+        header("Location: login.php");
+        exit(); // Stop further execution
     }
 
-    $conn->close();
-    ?>
+    $checkEmailStmt->close();
+
+    // If the email doesn't exist, proceed with registration
+    $registerStmt = $conn->prepare("INSERT INTO user (firstname, lastname, type, address, phoneNumber, username, password, emailAddress, ipAddress) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $registerStmt->bind_param("sssssssss", $firstName, $lastName, $role, $address, $phoneNumber, $userName, $password, $email, $ipAddress);
+
+    // Attempt to register
+    if ($registerStmt->execute()) {
+        // Retrieve client ID from the database (you may need to adjust this query based on your database schema)
+        $getClientIDStmt = $conn->prepare("SELECT userID FROM user WHERE emailAddress = ?");
+        $getClientIDStmt->bind_param("s", $email);
+        $getClientIDStmt->execute();
+        $clientIDResult = $getClientIDStmt->get_result();
+        
+        if ($clientIDRow = $clientIDResult->fetch_assoc()) {
+            $clientID = $clientIDRow['userID'];
+
+            // Store the client ID in the session
+            $_SESSION['client_id'] = $clientID;
+
+            echo "<p class='success'>User registered successfully!</p>";
+
+            // Redirect to the appropriate dashboard based on the user's role
+            switch ($role) {
+                case 'client':
+                    header("Location: client/clientDash.html");
+                    break;
+                case 'sitter':
+                    header("Location: sitter/sitterDash.html");
+                    break;
+                case 'handler':
+                    header("Location: handler/handlerDash.html");
+                    break;
+                default:
+                    // Redirect to a default page or display an error
+                    header("Location: landing.html");
+                    exit();
+            }
+        } else {
+            echo "<p class='error'>Error retrieving client ID.</p>";
+        }
+
+        $getClientIDStmt->close();
+    } else {
+        echo "<p class='error'>Error registering user: " . $registerStmt->error . "</p>";
+    }
+
+    $registerStmt->close();
+}
+
+$conn->close();
+?>
 </body>
 
 </html>
